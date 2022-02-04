fd43fb0e98512d4aa5342dae7563f83191e0becd
gabrieldpeterson <gabrieldpeterson@gmail.com>
2022-02-03 21:21:41 -0800

    Configure style loader and css loader, and add some test code to make sure things run

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import './style.css';
 import Logo from './logo.png';
 
-const content = document.querySelector('#content');
-const logoDiv = document.createElement('div');
-const logo = new Image();
-logo.src = Logo;
-logoDiv.appendChild(logo);
-content.appendChild(logoDiv);
+const domBuilder = (() => {
+  const buildSkeleton = () => {
+    const content = document.querySelector('#content');
+    const logoDiv = document.createElement('div');
+    const logo = new Image();
+    logo.src = Logo;
+    logoDiv.appendChild(logo);
+    content.appendChild(logoDiv);
+    const test = document.createElement('div');
+    test.textContent = 'Test';
+    logoDiv.appendChild(test);
+  };
+
+  return { buildSkeleton };
+})();
+
+domBuilder.buildSkeleton();
