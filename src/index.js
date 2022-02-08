a8ac456bdd96493d99f3061b1c49abeba798e1e3
gabrieldpeterson <gabrieldpeterson@gmail.com>
2022-02-07 20:17:47 -0800

    Replace temp skeleton with real skeleton

--- src/index.js
+++ src/index.js
@@ -3,16 +3,33 @@ import Logo from './logo.png';
 
 const domBuilder = (() => {
   const buildSkeleton = () => {
-    const content = document.querySelector('#content');
-    const logoDiv = document.createElement('div');
-    const logo = new Image();
-    logo.src = Logo;
-    logoDiv.appendChild(logo);
-    content.appendChild(logoDiv);
-    const test = document.createElement('div');
-    test.textContent = 'Test';
-    logoDiv.appendChild(test);
+    const container = document.querySelector('#container');
+
+    const navBar = document.createElement('div');
+    navBar.setAttribute('id', 'nav-bar');
+
+    const contentBox = document.createElement('div');
+    contentBox.setAttribute('id', 'main-content');
+
+    const content = document.createElement('div');
+    content.setAttribute('id', 'content');
+
+    const footer = document.createElement('div');
+    footer.setAttribute('id', 'footer');
+
+    container.appendChild(navBar);
+    container.appendChild(contentBox);
+    contentBox.appendChild(content);
+    container.appendChild(footer);
   };
+
+  const buildNavMenu = () => {
+    // todo
+  }
+
+  const fillContent = () => {
+    // todo
+  }
 
   return { buildSkeleton };
 })();
